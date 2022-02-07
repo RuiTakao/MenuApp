363f7f480c69ebd9921f1553a8315e042fcf357d
RuiTakao <ruia1082halfnc@gmail.com>
2022-02-07 16:49:41 +0900

    js変更

--- component_test/main.js
+++ component_test/main.js
@@ -8,21 +8,15 @@ $(function () {
     $('.recommendation_lists').hide().eq(index).show();
   });
 
-  //main_header_nav タブ アクション
-  // $(".food_menu_lists:not(:first-of-type)").css("display", "none");
-  // $('.main_header_nav_list').click(function () {
-  //   var index = $('.main_header_nav_list').index(this);
-  //   $('.main_header_nav_list').removeClass('current');
-  //   $(this).addClass('current');
-  //   $('.food_menu_lists').hide().eq(index).show();
-
-    // if($('.is-active').hasClass('current')){
-    //   $('main').addClass('header_nav_height');
-    //   $('.main_header_nav').addClass('add_header_nav');
-    // }
-    // if(!$('.is-active').hasClass('current')){
-    //   $('main').removeClass('header_nav_height');
-    //   $('.main_header_nav').removeClass('add_header_nav');
-    // }
-  // });
+  $('main_header_nav_lists_food > a[href^="#"]').click(function() {
+    // スクロールの速度
+    let speed = 400; // ミリ秒で記述
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
 });
